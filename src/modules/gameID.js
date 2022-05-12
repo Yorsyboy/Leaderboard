e1a328d2d93f75b0ae5a8e2342c9ee9d15b5a019
yorsyboy <adekunletaiwo43@yahoo.com>
2022-05-12 05:45:41 +0100

    fox linters

--- src/modules/gameID.js
+++ src/modules/gameID.js
@@ -4,10 +4,10 @@ export default class GameID {
     gameID = 'Zl4d7IVkemOTTVg2fUdz';
 
     addNewScore = (score) => {
-        this.scores.push(score);
+      this.scores.push(score);
     }
 
     clearArray = () => {
-        this.scores = this.scores.splice(0, this.scores.length);
+      this.scores = this.scores.splice(0, this.scores.length);
     }
 }
